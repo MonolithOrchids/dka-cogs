8da0c58e9701d23bc6202e4ceb876ff77e21d072
MonolithOrchids <uncannyrd@gmail.com>
2017-09-07 19:48:34 -0300

    Create nuuvem.py

---
+++ nuuvem/nuuvem.py
@@ -0,0 +1,18 @@
+from discord.ext import commands
+
+class Nuuvem:
+    """Procure por jogos na maior revendedora de jogos no Brasil"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command()
+    async def nuuvem(self, *text):
+        """Busca pelo jogo na Nuuvem, maior revendedora de jogos do Brasil"""
+
+        text = " ".join(text)
+        query=text.replace(" ", "%20")
+        await self.bot.say("https://www.nuuvem.com/catalog/search/"+query)
+
+def setup(bot):
+bot.add_cog(Nuuvem(bot))
